1f39666f794a1dfb4c4bce3bce5a602788898445
GibbOne <gpolzoni@tiscali.it>
2023-09-16 07:20:37 +0200

    Fix test

--- src/test/java/Object2DTests.java
+++ src/test/java/Object2DTests.java
@@ -6,14 +6,15 @@ import javafx.scene.shape.Rectangle;
 
 import static org.junit.Assert.assertTrue;
 import myWall.Object2D;
+import myWall.World;
 
 public class Object2DTests {
     
     @Test 
     public void ItShouldNotCollides() {
         //ARRANGE
-        var o1 = new MockObject2D(new Rectangle(5, 5, 10, 10), 1);
-        var o2 = new MockObject2D(new Circle(0, 0, 3), 2);
+        var o1 = new MockObject2D(null, new Rectangle(5, 5, 10, 10), 1, 0);
+        var o2 = new MockObject2D(null, new Circle(0, 0, 3), 2, 0);
 
         // ACT and ASSERT
         assertTrue(o1.getCollisionPointWith(o2) == null);
@@ -22,8 +23,8 @@ public class Object2DTests {
 }
 
 class MockObject2D extends Object2D {
-    public MockObject2D(Shape s, float mass) 
+    public MockObject2D(World w, Shape s, float mass, float elasticity) 
     {
-        super(s, mass);
+        super(w, s, mass, elasticity);
     }
 }
