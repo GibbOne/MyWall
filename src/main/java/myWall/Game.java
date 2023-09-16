51a76ecbc535a08026a4a7eee4c8e53361aa9b07
GibbOne <gpolzoni@tiscali.it>
2023-09-16 07:20:16 +0200

    Game added

---
+++ src/main/java/myWall/Game.java
@@ -0,0 +1,17 @@
+package myWall;
+
+import java.util.List;
+
+public class Game {
+    private World world = new World();
+
+    public Game() 
+    {
+        world.addObject(new Wall(world));
+    }
+
+    public List<Object2D>  getObjects()
+    {
+        return world.getObjects();
+    }
+}
